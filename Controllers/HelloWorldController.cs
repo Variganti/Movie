2cb4aaa572d9daf106a8e073266ec3008e06dc2a
Variganti <k.radha61@gmail.com>
2024-08-11 13:44:33 +0530

    add asp.net mvc

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace WebApplication9.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index(string name, int numTimes=4)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ Data/WebApplication9Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace WebApplication9.Data
+{
+    public class WebApplication9Context : DbContext
+    {
+        public WebApplication9Context (DbContextOptions<WebApplication9Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
